db9e58f341ca59bafcb6c5fbf478c1476399bfb8
Moataz Rabaya <mutazrabaya203@gmail.com>
2026-06-08 14:08:13 +0300

    Create TShirtDecorator class

---
+++ TShirtShop/Decorator/TShirtDecorator.cs
@@ -0,0 +1,17 @@
+using TShirtShop.Models;
+
+namespace TShirtShop.Decorator;
+
+public abstract class TShirtDecorator : TShirt
+{
+    protected TShirt _tshirt;
+
+    public TShirtDecorator(TShirt tshirt)
+    {
+        _tshirt = tshirt;
+    }
+
+    public abstract override string FullDescription {get;}
+
+    public override void FormSize() => _tshirt.FormSize();
+}
