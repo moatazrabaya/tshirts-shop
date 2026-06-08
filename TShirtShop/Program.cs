8bd3f65cb2d641355a86f8ddd20ac21c29947725
Moataz Rabaya <mutazrabaya203@gmail.com>
2026-06-08 20:08:09 +0300

    Update Program class, add simple test application

--- TShirtShop/Program.cs
+++ TShirtShop/Program.cs
@@ -1,2 +1,36 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using TShirtShop.AbstractFactory;
+using TShirtShop.Decorator;
+using TShirtShop.Models;
+using TShirtShop.Observer;
+using TShirtShop.Singleton;
+
+namespace TShirtShop;
+
+class Program
+{
+    static void Main(String[] args)
+    {
+        SalesTracker tracker = SalesTracker.Instance;
+
+        ShopOwner owner = new("Moataz");
+
+        tracker.RegisterObserver(owner);
+
+        TShirtFactory factory = new ChinaFactory();
+
+        TShirt tshirt = factory.CreateMedium();
+
+        tshirt = new LogoDecorator(tshirt);
+
+        tshirt = new BlueColorDecorator(tshirt);
+
+        Order order = Order.Instance;
+
+        order.PlaceOrder(tshirt);
+
+        order.ShowOrder();
+
+        order.CompleteOrder();
+
+    }
+}
