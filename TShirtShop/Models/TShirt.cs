f68a8175d665e0ff99b27220a2937e1ed9b3f8b0
Moataz Rabaya <mutazrabaya203@gmail.com>
2026-06-10 14:53:37 +0300

    Add Size and Region properties to TShirt base class

--- TShirtShop/Models/TShirt.cs
+++ TShirtShop/Models/TShirt.cs
@@ -3,7 +3,9 @@ namespace TShirtShop.Models;
 public abstract class TShirt
 {
     public string Name { get; set; }
-    public string Description { get; set; }
-    public virtual string FullDescription => $"{Name} - {Description}";
+    public string Size {get; set;}
+    public string Region {get; set;}
+    
+    public virtual string Description => $"{Name} - A {Size} T-shirt designed for the {Region} region";
     public abstract void FormSize();
 }
